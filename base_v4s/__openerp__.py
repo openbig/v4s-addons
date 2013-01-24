3b696cf4b7337dcedd53f4dea61cc44984f3a934
andrzejgrymkowski@gmail.com <>
2013-01-24 22:06:01 +0100

    modify headers in py files. Base and CRM modules are now independent

--- base_v4s/__openerp__.py
+++ base_v4s/__openerp__.py
@@ -1,26 +1,25 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#
+#    
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
-#    Module base_v4s is copyrighted by 
-#    Andrzej Grymkowski of OpenGLOBE (www.openglobe.pl)
-#    with the same rules as OpenObject and OpenERP platform
+#    Module base_v4s
+#    Copyrigt (C) 2010 OpenGLOBE Grzegorz Grzelak (www.openglobe.pl)
+#                       and big-consulting GmbH (www.openbig.de)
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
 
@@ -28,13 +27,13 @@
 {
     "name" : "V4S - BASE",
     "version" : "0.01 (6.1)",
-    "author" : "Grzegorz Grzelak & Andrzej Grymkowski",
-    "website": "http://www.openglobe.pl",
+    "author" : "Grzegorz Grzelak / Thorsten Vocks for openbig.org",
+    "website": "http://www.openbig.org",
     "category" : "Localisation/Country specific stuff",
     "description": """
-    
+    Added new fields to customer view.
     """,
-    "depends" : ["crm"],
+    "depends" : [],
     "demo_xml" : [],
     "update_xml" : [
                     "res_partner_view.xml",
--- crm_v4s/__openerp__.py
+++ crm_v4s/__openerp__.py
@@ -1,26 +1,25 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#
+#    
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
-#    Module crm_v4s is copyrighted by 
-#    Andrzej Grymkowski of OpenGLOBE (www.openglobe.pl)
-#    with the same rules as OpenObject and OpenERP platform
+#    Module crm_v4s
+#    Copyrigt (C) 2010 OpenGLOBE Grzegorz Grzelak (www.openglobe.pl)
+#                       and big-consulting GmbH (www.openbig.de)
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
 
@@ -28,16 +27,17 @@
 {
     "name" : "V4S - CRM",
     "version" : "0.01 (6.1)",
-    "author" : "Grzegorz Grzelak & Andrzej Grymkowski",
-    "website": "http://www.openglobe.pl",
+    "author" : "Grzegorz Grzelak / Thorsten Vocks for openbig.org",
+    "website": "http://www.openbig.org",
     "category" : "Localisation/Country specific stuff",
     "description": """
-    
+    Added new fields for CRM and Customer views.
     """,
     "depends" : ["crm"],
     "demo_xml" : [],
     "update_xml" : [
                     "crm_lead_view.xml",
+                    "res_partner_view.xml",
                     ],
     "active": False,
     "installable": True
