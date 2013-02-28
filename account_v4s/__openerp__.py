16387c969dac00a14cc3d9b6ab4349dea6bc2bf5
Grzegorz Grzelak <grzegorz.grzelak@openglobe.pl>
2013-02-28 12:13:14 +0100

    account_v4s, purchase_v4s and stock_v4s added

---
+++ account_v4s/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Module account_v4s
+#    Copyrigt (C) 2010 OpenGLOBE Grzegorz Grzelak (www.openglobe.pl)
+#                      and big-consulting GmbH (www.openbig.de)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    "name" : "V4S - Account",
+    "version" : "0.01 (6.1)",
+    "author" : "Grzegorz Grzelak / Thorsten Vocks for openbig.org",
+    "website": "http://www.openbig.org",
+    "category" : "Localisation/Country specific stuff",
+    "description": """
+    Account customization for via4spine.
+    * invoice report changed
+    """,
+    "depends" : ["account"],
+    "demo_xml" : [],
+    "update_xml" : [
+            "report/account_print_invoice.xml",
+                    ],
+    "active": False,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ stock_v4s/report/picking.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Module stock_v4s
+#    Copyrigt (C) 2010 OpenGLOBE Grzegorz Grzelak (www.openglobe.pl)
+#                       and big-consulting GmbH (www.openbig.de)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from report import report_sxw
+from osv import osv
+import pooler
+
+class picking(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(picking, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'get_qtytotal':self._get_qtytotal
+        })
+    def _get_qtytotal(self,move_lines):
+        total = 0.0
+        uom = move_lines[0].product_uom.name
+        for move in move_lines:
+            total+=move.product_qty
+        return {'quantity':total,'uom':uom}
+
+report_sxw.report_sxw('report.stock_v4s.picking_v4s','stock.picking','addons/stock_v4s/report/picking.rml',parser=picking)
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
