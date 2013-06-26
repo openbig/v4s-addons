77f59311eb26a66cfa99ccbf4a89aa93dfa9e4df
Andrzej Grymkowski <andrzej.grymkowski@openglobe.pl>
2013-06-26 16:28:24 +0200

    stylesheet, reports, fixed translations

--- sale_v4s/__openerp__.py
+++ sale_v4s/__openerp__.py
@@ -28,7 +28,7 @@
 
 {
     "name" : "V4S - SALE",
-    "version" : "0.09 (6.1)",
+    "version" : "0.10 (6.1)",
     "author" : "Grzegorz Grzelak / Thorsten Vocks for openbig.org",
     "website": "http://www.openbig.org",
     "category" : "Localisation/Country specific stuff",
