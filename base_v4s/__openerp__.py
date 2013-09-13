33e311cd907a882c9f37812791cedbc8bb2afe25
Andrzej Grymkowski <andrzej.grymkowski@openglobe.pl>
2013-09-13 14:53:37 +0200

    changes in stylesheet and stock picking report

--- base_v4s/__openerp__.py
+++ base_v4s/__openerp__.py
@@ -26,7 +26,7 @@
 
 {
     "name" : "V4S - BASE",
-    "version" : "0.11 (6.1)",
+    "version" : "0.12 (6.1)",
     "author" : "Grzegorz Grzelak / Thorsten Vocks for openbig.org",
     "website": "http://www.openbig.org",
     "category" : "Localisation/Country specific stuff",
