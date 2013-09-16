ad0f97b163d55e397b5461e0808d8979c9a4d56d
Andrzej Grymkowski <andrzej.grymkowski@openglobe.pl>
2013-09-16 14:46:44 +0200

    from mail: v4s->partly solved. Service product can not be procurement_method 'make_to_order'

--- sale_v4s/sale.py
+++ sale_v4s/sale.py
@@ -50,8 +50,9 @@ class sale_order(osv.osv):
         if shop_brw.set_mto == True:
             for sale in self.browse(cr, uid , ids):
                 for line in sale.order_line:
-                    self.pool.get('sale.order.line').write(cr, uid, line.id, { 'type' : 'make_to_order' })
-        
+                    if line.product_id:
+                        if line.product_id.type!='service':
+                            self.pool.get('sale.order.line').write(cr, uid, line.id, { 'type' : 'make_to_order' })
         return res
     
     
@@ -59,7 +60,7 @@ class sale_order(osv.osv):
         invoice_vals = super(sale_order, self)._prepare_invoice(cr, uid, order, lines, context=context)
         if invoice_vals['type'] in ('out_invoice', 'out_refund') and not invoice_vals.has_key('client_order_ref'):
             invoice_vals['client_order_ref'] = invoice_vals['name']
-        print 'Sale', invoice_vals
+
         return invoice_vals
     
 sale_order()    
@@ -68,20 +69,49 @@ class sale_order_line(osv.osv):
     _inherit = "sale.order.line"
     
     def create(self, cr, uid, vals, context=None):
+        product_id = vals.get('product_id', None)
         order_id = vals.get('order_id')
         if not order_id: return super(sale_order_line, self).create(cr, uid, vals, context)
 
+        if product_id:
+            product_brw = self.pool.get('product.product').browse(cr, uid, product_id, context=context)
         order_brw = self.pool.get('sale.order').browse(cr, uid, order_id, context=context)
-        mto = order_brw.shop_id.set_mto
+        shop_id = order_brw.shop_id
 
-        if mto==True: 
-            vals.update(
-            {
-                'type' : 'make_to_order'
-            })
-                        
+        type = vals.get('type', None)
+        
+        if product_id and product_brw.type=='service':
+            type = 'make_to_stock'
+        elif shop_id.set_mto==True: 
+            type = 'make_to_order'
+            
+        if type:
+            vals['type'] = type
         return super(sale_order_line, self).create(cr, uid, vals, context)
 
+
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
+
+        res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty,
+            uom, qty_uos, uos, name, partner_id,
+            lang, update_tax, date_order, packaging, fiscal_position, flag, context)
+        
+        vals = {}
+        if not product:
+            return res
+        
+        product_brw = self.pool.get('product.product').browse(cr, uid, product, context=context)
+        procure_method = res['value'].get('type', 'None')
+        
+        # correct procurement method. We cant apply make_to_order when product is service
+        if product_brw.type=='service' and procure_method=='make_to_order':
+            vals['type'] = 'make_to_stock'
+            
+        res['value'].update(vals)
+        return res
+        
 sale_order_line()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
